c9badd8b6fe413dfc3f8ec36def1e4c94b3c11e8
Gui0403 <pabloguilhermemartinez@gmail.com>
2025-02-12 16:48:51 -0400

    implementando um gerenciador de rotas

---
+++ app/Http/Router.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http;
+
+class Router
+{
+
+    /**
+     * URL completa do projeto (raiz)
+     * @var string
+     */
+    private $url = '';
+
+    /**
+     * Prefixo de todas as rotas
+     * @var string
+     */
+    private $prefix = '';
+
+    /**
+     * Índice de rotas
+     * @var array
+     */
+    private $routes = [];
+
+    /**
+     * Instacia de request
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * Método responsável por iniciar a classe
+     * @param string $url
+     */
+    public function __construct($url)
+    {
+        $this->request = new Request();
+        $this->url = $url;
+    }
+}
--- index.php
+++ index.php
@@ -4,6 +4,4 @@ require __DIR__."/vendor/autoload.php";
 
 use \App\Controller\Pages\Home;
 
-$home = new Home();
-
 echo Home::getHome();
